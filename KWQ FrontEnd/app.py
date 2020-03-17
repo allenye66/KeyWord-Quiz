849cdb4fed78b89e22fe2be2718381a36d9b21e7
Allen Ye <46653284+allenye66@users.noreply.github.com>
2020-03-16 19:41:39 -0700

    integrate flask + simple_np

--- KWQ FrontEnd/app.py
+++ KWQ FrontEnd/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+import nltk
+nltk.download('all')
+from np_extractor import get_nps_from_text, get_nps_from_tokens
+from nltk import tokenize
+
 
 app = Flask(__name__)
 
@@ -10,12 +15,20 @@ def index():
 @app.route('/show', methods = ['GET' ,'POST'])
 def show():
 	t = request.form['txt']
+	arr = tokenize.sent_tokenize(t)
+	booga = ""
+	for sentence in arr:
+		keywords = get_nps_from_text(sentence)
+		for words in keywords:
+			sentence = sentence.replace(words, '___')
+		booga = booga + sentence
+
 	'''
 	age = request.form['age']
 	db = request.form ['dateofbirth']
 	print("asdfadfasdfasdffd")
 	'''
-	return render_template('pass.html', txt = t)
+	return render_template('pass.html', txt = booga)
 
 if __name__ == '__main__':
 	app.run(debug=True)
